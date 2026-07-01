68c3f50b29870b6153b807f072a927f73b93ea5b
madebyandrea13 <andrea@madebyandrea.it>
2026-07-01 20:12:11 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const CACHE = 'trambusto-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
